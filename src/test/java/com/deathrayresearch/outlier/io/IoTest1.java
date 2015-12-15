92c9ac3b66d028b876752475161cd69ab2b86287
Larry White <ljw1001@gmail.com>
2015-12-15 10:52:31 -0500

    new test, new test data

---
+++ src/test/java/com/deathrayresearch/outlier/io/IoTest1.java
@@ -0,0 +1,29 @@
+package com.deathrayresearch.outlier.io;
+
+import com.deathrayresearch.outlier.ColumnType;
+import com.deathrayresearch.outlier.Table;
+import org.junit.Test;
+
+import static com.deathrayresearch.outlier.ColumnType.*;
+
+/**
+ *
+ */
+public class IoTest1 {
+
+  @Test
+  public void testWithBushData() throws Exception {
+    // Read the CSV file
+    ColumnType[] types = {FLOAT, TEXT, TEXT, FLOAT, FLOAT};
+    Table table = CsvReader.read("data/bus_stop_test.csv", types);
+
+    // Look at the column names
+    print(table.columnNames());
+
+    print(table.print());
+  }
+
+  private void print(Object o) {
+    System.out.println(o);
+  }
+}
