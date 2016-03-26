df89bddb66fa3eca0a59bffd1ecaffc643e60ad1
Larry White <ljw1001@gmail.com>
2016-03-26 09:53:44 -0400

    removed two unused files from web ui experiment

--- Script.js
+++
@@ -1,8 +0,0 @@
-var fun1 = function(name) {
-    print('Hi there from Javascript, ' + name);
-    return "greetings from javascript";
-};
-
-var fun2 = function (object) {
-    print("JS Class Definition: " + Object.prototype.toString.call(object));
-};
