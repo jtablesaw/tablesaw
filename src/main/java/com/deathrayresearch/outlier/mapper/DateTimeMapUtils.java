44d0d001e9e7bdd66da955d43061a734364f9ed5
lwhite1 <lwhite@tracelink.com>
2016-03-26 22:05:07 -0400

    minor adjustments to DateTimeMapUtils and tests for its methods

--- src/main/java/com/deathrayresearch/outlier/mapper/DateTimeMapUtils.java
+++ src/main/java/com/deathrayresearch/outlier/mapper/DateTimeMapUtils.java
@@ -32,33 +32,35 @@ public interface DateTimeMapUtils extends Column {
   }
 
   default LongColumn difference(LocalDateTimeColumn column2, ChronoUnit unit) {
-
     LongColumn newColumn = LongColumn.create(name() + " - " + column2.name());
+
     for (int r = 0; r < size(); r++) {
       long c1 = this.getLong(r);
       long c2 = column2.getLong(r);
       if (c1 == LocalDateTimeColumn.MISSING_VALUE || c2 == LocalDateTimeColumn.MISSING_VALUE) {
         newColumn.add(IntColumn.MISSING_VALUE);
       } else {
-        LocalDateTime value1 = PackedLocalDateTime.asLocalDateTime(c1);
-        LocalDateTime value2 = PackedLocalDateTime.asLocalDateTime(c2);
-        newColumn.add(unit.between(value1, value2));
+        newColumn.add(difference(c1, c2, unit));
       }
     }
     return newColumn;
   }
 
-  default CategoryColumn hour() {
-    ChronoUnit unit = ChronoUnit.HOURS;
-    CategoryColumn newColumn = CategoryColumn.create(name() + "[" + unit.name() + "]");
+  default long difference(long packedLocalDateTime1, long packedLocalDateTime2, ChronoUnit unit) {
+    LocalDateTime value1 = PackedLocalDateTime.asLocalDateTime(packedLocalDateTime1);
+    LocalDateTime value2 = PackedLocalDateTime.asLocalDateTime(packedLocalDateTime2);
+    return unit.between(value1, value2);
+  }
+
+  default ShortColumn hour() {
+    ShortColumn newColumn = ShortColumn.create(name() + "[" + "hour" + "]");
     for (int r = 0; r < size(); r++) {
-      LocalDateTime c1 = get(r);
-      String value;
-      if (c1 != null) {
-        value = String.valueOf(c1.getHour());
-        newColumn.add(value);
+      long c1 = getLong(r);
+      if (c1 != LocalDateTimeColumn.MISSING_VALUE) {
+        newColumn.add(PackedLocalDateTime.getHour(c1));
+      } else {
+        newColumn.add(ShortColumn.MISSING_VALUE);
       }
-        newColumn.add(CategoryColumn.MISSING_VALUE);
     }
     return newColumn;
   }
