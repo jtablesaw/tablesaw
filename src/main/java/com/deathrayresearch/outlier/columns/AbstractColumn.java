f420775eb1cd9bc02931828b996ee257b31727aa
lwhite1 <lwhite@tracelink.com>
2016-03-30 15:37:38 -0400

    implemented improved column functionality, including ChangeLog framework

--- src/main/java/com/deathrayresearch/outlier/columns/AbstractColumn.java
+++ src/main/java/com/deathrayresearch/outlier/columns/AbstractColumn.java
@@ -1,8 +1,8 @@
 package com.deathrayresearch.outlier.columns;
 
 import com.deathrayresearch.outlier.store.ColumnMetadata;
-import it.unimi.dsi.fastutil.ints.IntArrayList;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -13,6 +13,12 @@ abstract class AbstractColumn implements Column {
   private String id;
 
   private String name;
+
+  private String comment;
+
+  private ColumnType columnType;
+
+  private ChangeLog changeLog = new ChangeLog();
 
   public AbstractColumn(String name) {
     this.name = name;
@@ -37,15 +43,32 @@ abstract class AbstractColumn implements Column {
     return new ColumnMetadata(this).toJson();
   }
 
-  @Override
-  public void sortOn(IntArrayList rows) {
-
-  }
-
   public void setName(String name) {
     this.name = name;
   }
 
   public abstract void addCell(String stringvalue);
 
+  public List<ChangeLog.ChangeLogEntry> getChangeLog() {
+    return changeLog.getEntries();
+  }
+
+  @Override
+  public String comment() {
+    return comment;
+  }
+
+  @Override
+  public ColumnType type() {
+    return columnType;
+  }
+
+  protected void setColumnType(ColumnType columnType) {
+    this.columnType = columnType;
+  }
+
+  @Override
+  public void setComment(String comment) {
+    this.comment = comment;
+  }
 }
