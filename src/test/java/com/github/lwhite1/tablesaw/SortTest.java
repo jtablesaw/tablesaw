7dbd4b7efb9eee2d50a2c9d5ad4c25d5eb5a5a70
Larry White <ljw1001@gmail.com>
2016-06-18 19:21:10 -0400

    Partial implementation of RDBMS integration. More tests needed (and a DB free way to execute as unit tests

--- src/test/java/com/github/lwhite1/tablesaw/SortTest.java
+++ src/test/java/com/github/lwhite1/tablesaw/SortTest.java
@@ -1,9 +1,12 @@
 package com.github.lwhite1.tablesaw;
 
+import com.github.lwhite1.tablesaw.api.ColumnType;
 import com.github.lwhite1.tablesaw.api.Table;
 import com.github.lwhite1.tablesaw.columns.CategoryColumn;
+import com.github.lwhite1.tablesaw.columns.IntColumn;
+import com.github.lwhite1.tablesaw.columns.LocalDateColumn;
+import com.github.lwhite1.tablesaw.io.CsvReader;
 import com.github.lwhite1.tablesaw.sorting.Sort;
-import com.github.lwhite1.tablesaw.store.StorageManager;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
 import it.unimi.dsi.fastutil.ints.IntArrays;
 import it.unimi.dsi.fastutil.ints.IntComparator;
@@ -13,44 +16,53 @@ import org.junit.Test;
 
 import java.util.Collections;
 
-import static org.junit.Assert.assertTrue;
+import static com.github.lwhite1.tablesaw.api.ColumnType.*;
 
 /**
  *
  */
 public class SortTest {
 
+  private ColumnType[] types = {
+      LOCAL_DATE,     // date of poll
+      INTEGER,        // approval rating (pct)
+      CATEGORY        // polling org
+  };
+
   private Table table1;
   private Table table2;
-
   private CategoryColumn column1;
-  private CategoryColumn column2;
+  private IntColumn column2;
+  private LocalDateColumn column3;
 
   @Before
   public void setUp() throws Exception {
-    table1 = StorageManager.readTable("bigdata/3f07b9bf-053f-4f9b-9dff-9d354835b276");
-    table2 = StorageManager.readTable("bigdata/3f07b9bf-053f-4f9b-9dff-9d354835b276");
-
-    column1 = table1.categoryColumn("ORIGIN");
-    column2 = table2.categoryColumn("ORIGIN");
+    table1 = CsvReader.read(types, "data/BushApproval.csv");
+    table2 = CsvReader.read(types, "data/BushApproval.csv");
+    column1 = table1.categoryColumn("who");
+    column2 = table1.intColumn("approval");
+    column3 = table1.localDateColumn("date");
   }
+
 
   @Ignore
   @Test
   public void sort() {
 
-    Sort key = Table.getSort("ORIGIN");
+    Sort key = Table.getSort("who");
     IntComparator rowComparator = table1.getComparator(key);
 
+    // sort the table and then
+    table1 = table1.sortOn(key);
     int[] t1rows = table1.rows();
-    IntArrayList rows1 = IntArrayList.wrap(t1rows);
-    Collections.sort(rows1, rowComparator);
 
     IntComparator rowComparator2 = table2.getComparator(key);
     int[] values = table2.rows();
 
+    System.out.println(table1.print());
+
     IntArrays.mergeSort(values, 0, values.length, rowComparator2);
-    assertTrue(java.util.Arrays.equals(values, rows1.elements()));
+   // assertTrue(java.util.Arrays.equals(values, t1rows));
   }
 
   @Ignore
@@ -63,9 +75,11 @@ public class SortTest {
     IntArrayList t1rows = IntArrayList.wrap(column1.indexes());
     Collections.sort(t1rows, rowComparator1);
 
+/*
     int[] values = column2.indexes();
 
     IntArrays.mergeSort(values, 0, values.length, rowComparator2);
     assertTrue(java.util.Arrays.equals(values, t1rows.elements()));
+*/
   }
 }
