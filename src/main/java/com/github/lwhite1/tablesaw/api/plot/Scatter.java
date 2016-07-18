32f7b5e8d1868499f699a30e59b0952529e78452
Larry White <ljw1001@gmail.com>
2016-07-17 20:22:37 -0400

    mods to Scatter plot

--- src/main/java/com/github/lwhite1/tablesaw/api/plot/Scatter.java
+++ src/main/java/com/github/lwhite1/tablesaw/api/plot/Scatter.java
@@ -1,33 +1,34 @@
 package com.github.lwhite1.tablesaw.api.plot;
 
 import com.github.lwhite1.tablesaw.api.NumericColumn;
+import com.github.lwhite1.tablesaw.api.plot.swing.TablesawTheme;
 import org.knowm.xchart.SwingWrapper;
 import org.knowm.xchart.XYChart;
 import org.knowm.xchart.XYSeries;
 import org.knowm.xchart.style.markers.SeriesMarkers;
 
-import java.awt.*;
-
 /**
- *
+ * Render scatter plots using Swing
  */
 public class Scatter {
 
   public static void show(String chartTitle, NumericColumn xColumn, NumericColumn yColumn) {
+    show(chartTitle, xColumn, yColumn, 600, 400);
+  }
+
+  public static void show(String chartTitle, NumericColumn xColumn, NumericColumn yColumn, int width, int height) {
     double[] xData = xColumn.toDoubleArray();
     double[] yData = yColumn.toDoubleArray();
 
     // Create Chart
-    XYChart chart = new XYChart(600, 400);
+    XYChart chart = new XYChart(width, height);
     chart.setTitle(chartTitle);
     chart.setXAxisTitle(xColumn.name());
     chart.setYAxisTitle(yColumn.name());
+    chart.getStyler().setTheme(new TablesawTheme());
+    chart.getStyler().setMarkerSize(2);
     chart.getStyler().setDefaultSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Scatter);
-    chart.getStyler().setChartTitleVisible(true);
-    chart.getStyler().setMarkerSize(1);
-    chart.getStyler().setLegendVisible(false);
-    chart.getStyler().setPlotBorderColor(Color.LIGHT_GRAY);
-    chart.getStyler().setChartBackgroundColor(Color.WHITE);
+
     XYSeries series = chart.addSeries(yColumn.name() + " by " + xColumn.name(), xData, yData);
     series.setMarker(SeriesMarkers.CIRCLE);
     new SwingWrapper<>(chart).displayChart();
