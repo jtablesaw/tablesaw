f4be95b0faa9dd13af36811305f9327190255fc6
Larry White <ljw1001@gmail.com>
2016-10-31 12:40:10 -0400

    added float index support and test

---
+++ src/main/java/com/github/lwhite1/tablesaw/index/FloatIndex.java
@@ -0,0 +1,89 @@
+package com.github.lwhite1.tablesaw.index;
+
+import com.github.lwhite1.tablesaw.api.FloatColumn;
+import com.github.lwhite1.tablesaw.util.BitmapBackedSelection;
+import com.github.lwhite1.tablesaw.util.Selection;
+import it.unimi.dsi.fastutil.floats.Float2ObjectAVLTreeMap;
+import it.unimi.dsi.fastutil.floats.Float2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.floats.Float2ObjectSortedMap;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+
+/**
+ * An index for four-byte floating point columns
+ */
+public class FloatIndex {
+
+  private final Float2ObjectAVLTreeMap<IntArrayList> index;
+
+  public FloatIndex(FloatColumn column) {
+    int sizeEstimate = Integer.min(1_000_000, column.size() / 100);
+    Float2ObjectOpenHashMap<IntArrayList> tempMap = new Float2ObjectOpenHashMap<>(sizeEstimate);
+    for (int i = 0; i < column.size(); i++) {
+      float value = column.get(i);
+      IntArrayList recordIds = tempMap.get(value);
+      if (recordIds == null) {
+        recordIds = new IntArrayList();
+        recordIds.add(i);
+        tempMap.trim();
+        tempMap.put(value, recordIds);
+      } else {
+        recordIds.add(i);
+      }
+    }
+    index = new Float2ObjectAVLTreeMap<>(tempMap);
+  }
+
+  /**
+   * Returns a bitmap containing row numbers of all cells matching the given int
+   *
+   * @param value This is a 'key' from the index perspective, meaning it is a value from the standpoint of the column
+   */
+  public Selection get(float value) {
+    Selection selection = new BitmapBackedSelection();
+    IntArrayList list = index.get(value);
+    addAllToSelection(list, selection);
+    return selection;
+  }
+
+  public Selection atLeast(float value) {
+    Selection selection = new BitmapBackedSelection();
+    Float2ObjectSortedMap<IntArrayList> tail = index.tailMap(value);
+    for (IntArrayList keys : tail.values()) {
+      addAllToSelection(keys, selection);
+    }
+    return selection;
+  }
+
+  public Selection greaterThan(float value) {
+    Selection selection = new BitmapBackedSelection();
+    Float2ObjectSortedMap<IntArrayList> tail = index.tailMap(value + 0.000001f);
+    for (IntArrayList keys : tail.values()) {
+      addAllToSelection(keys, selection);
+    }
+    return selection;
+  }
+
+  public Selection atMost(float value) {
+    Selection selection = new BitmapBackedSelection();
+    Float2ObjectSortedMap<IntArrayList> head = index.headMap(value + 0.000001f);  // we add 1 to get values equal to the arg
+    for (IntArrayList keys : head.values()) {
+      addAllToSelection(keys, selection);
+    }
+    return selection;
+  }
+
+  public Selection lessThan(float value) {
+    Selection selection = new BitmapBackedSelection();
+    Float2ObjectSortedMap<IntArrayList> head = index.headMap(value);  // we add 1 to get values equal to the arg
+    for (IntArrayList keys : head.values()) {
+      addAllToSelection(keys, selection);
+    }
+    return selection;
+  }
+
+  private static void addAllToSelection(IntArrayList tableKeys, Selection selection) {
+    for (int i : tableKeys) {
+      selection.add(i);
+    }
+  }
+}
