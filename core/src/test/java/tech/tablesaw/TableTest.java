5dc46e3fc2d9bf9bfaac9cdead6549c03ef6816c
ustits <ustitsruslan@gmail.com>
2017-09-07 23:22:45 +0300

    #44 tests for Table#append

--- core/src/test/java/tech/tablesaw/TableTest.java
+++ core/src/test/java/tech/tablesaw/TableTest.java
@@ -8,16 +8,23 @@ import tech.tablesaw.api.FloatColumn;
 import tech.tablesaw.api.Table;
 import tech.tablesaw.columns.Column;
 
+import java.util.Random;
+
 import static org.junit.Assert.*;
+import static org.junit.Assume.assumeFalse;
+import static org.junit.Assume.assumeTrue;
 
 /**
  * Tests for Table
  */
 public class TableTest {
 
+    private static final int ROWS_BOUNDARY = 1000;
+
     private Table table;
     private FloatColumn floatColumn = new FloatColumn("f1");
     private DoubleColumn doubleColumn = new DoubleColumn("d1");
+    private Random random = new Random();
 
     @Before
     public void setUp() throws Exception {
@@ -63,5 +70,100 @@ public class TableTest {
         assertEquals(1, table.rowCount());
         floatColumn.append(2.2342f);
         assertEquals(2, table.rowCount());
+    }
+
+    @Test
+    public void testAppend() throws Exception {
+        int appendSize = random.nextInt(ROWS_BOUNDARY);
+        appendTableWithSingleColumn(appendSize);
+    }
+
+    @Test
+    public void testAppendEmptyTable() throws Exception {
+        appendTableWithSingleColumn(0);
+    }
+
+    @Test
+    public void testAppendToNonEmptyTable() throws Exception {
+        int appendSize = random.nextInt(ROWS_BOUNDARY);
+        populateColumn(floatColumn);
+        assumeFalse(table.isEmpty());
+        appendTableWithSingleColumn(appendSize);
+    }
+
+    @Test
+    public void testAppendEmptyTableToNonEmptyTable() throws Exception {
+        populateColumn(floatColumn);
+        assumeFalse(table.isEmpty());
+        appendTableWithSingleColumn(0);
+    }
+
+    @Test
+    public void testAppendMultipleColumns() throws Exception {
+        FloatColumn column = new FloatColumn("e1");
+        table.addColumn(column);
+        FloatColumn first = floatColumn.emptyCopy();
+        FloatColumn second = column.emptyCopy();
+        int firstColumnSize = populateColumn(first);
+        int secondColumnSize = populateColumn(second);
+        Table tableToAppend = Table.create("populated", first, second);
+        table.append(tableToAppend);
+        assertColumnSize(floatColumn, firstColumnSize);
+        assertColumnSize(column, secondColumnSize);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testAppendNull() throws Exception {
+        table.append(null);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testAppendTableWithNonExistingColumns() throws Exception {
+        Table tableToAppend = Table.create("wrong", doubleColumn);
+        table.append(tableToAppend);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testAppendTableWithAnotherColumnName() throws Exception {
+        FloatColumn column = new FloatColumn("42");
+        Table tableToAppend = Table.create("wrong", column);
+        table.append(tableToAppend);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testAppendTableWithDifferentShape() throws Exception {
+        FloatColumn column = new FloatColumn("e1");
+        table.addColumn(column);
+        Table tableToAppend = Table.create("different", column);
+        assumeTrue(table.columns().size() == 2);
+        assumeTrue(tableToAppend.columns().size() == 1);
+        table.append(tableToAppend);
+    }
+
+    private void appendTableWithSingleColumn(final int rowsToAppend) {
+        FloatColumn column = floatColumn.emptyCopy();
+        populateColumn(column, rowsToAppend);
+        Table tableToAppend = Table.create("populated", column);
+        int initialSize = table.rowCount();
+        table.append(tableToAppend);
+        assertColumnSize(floatColumn, initialSize + rowsToAppend);
+    }
+
+    private void assertColumnSize(Column column, int expected) {
+        int actual = table.column(column.name()).size();
+        assertEquals(expected, actual);
+    }
+
+    private int populateColumn(final FloatColumn floatColumn) {
+        int rowsCount = random.nextInt(ROWS_BOUNDARY);
+        return populateColumn(floatColumn, rowsCount);
+    }
+
+    private int populateColumn(final FloatColumn floatColumn, final int rowsCount) {
+        for (int i = 0; i < rowsCount; i++) {
+            floatColumn.append(random.nextFloat());
+        }
+        assumeTrue(floatColumn.size() == rowsCount);
+        return rowsCount;
     }
 }
