fe64bd096da04552071a17199032f44d9f1c13bb
Larry White <ljw1001@gmail.com>
2018-03-13 08:11:16 -0400

    added a variation of get(row, column) to table that allows column to be specified by name, improving readability.

--- core/src/main/java/tech/tablesaw/api/Table.java
+++ core/src/main/java/tech/tablesaw/api/Table.java
@@ -417,6 +417,21 @@ public class Table extends Relation implements IntIterable {
     }
 
     /**
+     * Returns a string representation of the value at the given row and column indexes
+     *
+     * @param r the row index, 0 based
+     * @param columnName the name of the column to be returned
+     *
+     * // TODO: performance would be greatly enhanced if columns could be referenced via a hashTable
+     */
+    public String get(int r, String columnName) {
+        Column column = column(columnIndex(columnName));
+        return column.getString(r);
+    }
+
+
+
+    /**
      * Returns a table with the same columns as this table
      */
     public Table fullCopy() {
