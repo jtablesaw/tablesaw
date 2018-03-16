e7bc8a78d111cb49bcb1b8e6b290848025d53833
Larry White <lwhite@foundationmedicine.com>
2018-03-16 10:02:13 -0400

    fix for test broken with last commit

--- core/src/test/java/tech/tablesaw/table/ViewGroupTest.java
+++ core/src/test/java/tech/tablesaw/table/ViewGroupTest.java
@@ -90,7 +90,7 @@ public class ViewGroupTest {
         Table t = table.sum("approval").by(splitColumnNames);
 
         // compare the sum of the original column with the sum of the sums of the group table
-        assertEquals(table.intColumn(1).sum(), Math.round(t.doubleColumn(2).sum()));
+        assertEquals(table.shortColumn(1).sum(), Math.round(t.doubleColumn(2).sum()));
         assertEquals(65, tables.size());
     }
 
@@ -114,6 +114,6 @@ public class ViewGroupTest {
     public void testSumGroup() {
         Table groups = table.sum("approval").by("who");
         // compare the sum of the original column with the sum of the sums of the group table
-        assertEquals(table.intColumn(1).sum(), Math.round(groups.doubleColumn(1).sum()));
+        assertEquals(table.shortColumn(1).sum(), Math.round(groups.doubleColumn(1).sum()));
     }
 }
