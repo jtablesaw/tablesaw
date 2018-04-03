2d945b4b5c8e2ea40879e29f169164da06ac6d04
Larry White <ljw1001@gmail.com>
2018-04-03 15:48:25 -0400

    remove unused and half-backed splitter framework. new splitting on calculated columns makes this approach superfluous

--- core/src/main/java/tech/tablesaw/splitting/Classification.java
+++
@@ -1,9 +0,0 @@
-package tech.tablesaw.splitting;
-
-import tech.tablesaw.api.Row;
-import tech.tablesaw.columns.Column;
-
-public interface Classification {
-
-    Object cut(Row row, Column column);
-}
