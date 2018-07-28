e408bd443b1b68eea31d9e4f0f81d333ff5ca51c
Larry White <ljw1001@gmail.com>
2018-07-28 11:34:00 -0400

    Heatmap trace basic implementation

---
+++ jsplot/src/main/java/tech/tablesaw/plotly/traces/HeatmapTrace.java
@@ -0,0 +1,80 @@
+package tech.tablesaw.plotly.traces;
+
+import com.mitchellbosecke.pebble.error.PebbleException;
+import com.mitchellbosecke.pebble.template.PebbleTemplate;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.List;
+import java.util.Map;
+
+import static tech.tablesaw.plotly.Utils.dataAsString;
+
+public class HeatmapTrace extends AbstractTrace {
+
+    private final Object[] x;
+    private final Object[] y;
+    private final double[][] z;
+    private final String type;
+
+    private HeatmapTrace(HeatmapBuilder builder) {
+        super(builder);
+        this.x = builder.x;
+        this.y = builder.y;
+        this.z = builder.z;
+        this.type = builder.getType();
+    }
+
+    @Override
+    public String asJavascript(int i) {
+        Writer writer = new StringWriter();
+        PebbleTemplate compiledTemplate;
+
+        try {
+            compiledTemplate = engine.getTemplate("trace_template.html");
+            compiledTemplate.evaluate(writer, getContext());
+        } catch (PebbleException | IOException e) {
+            e.printStackTrace();
+        }
+        return writer.toString();
+    }
+
+    protected Map<String, Object> getContext() {
+
+        Map<String, Object> context = super.getContext();
+        context.put("variableName", "trace0");
+        context.put("x", dataAsString(x));
+        context.put("y", dataAsString(y));
+        context.put("z", dataAsString(z));
+        context.put("type", type);
+        return context;
+    }
+
+    public static HeatmapBuilder heatmapBuilder(List<String> x, Object[] y, double[][] z) {
+        return new HeatmapBuilder(x, y, z);
+    }
+
+    public static class HeatmapBuilder extends TraceBuilder {
+
+        private static final String type = "heatmap";
+        private Object[] x;
+        private Object[] y;
+        private double[][] z;
+
+        HeatmapBuilder(List<String> x, Object[] y, double[][] z) {
+            this.x = x.toArray(new String[0]);
+            this.y = y;
+            this.z = z;
+        }
+
+        public HeatmapTrace build() {
+            return new HeatmapTrace(this);
+        }
+
+        @Override
+        protected String getType() {
+            return type;
+        }
+    }
+}
