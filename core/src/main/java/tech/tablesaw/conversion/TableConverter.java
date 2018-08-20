319a4d9c31ad65e14eb297cbd1204929fc132d44
Larry White <ljw1001@gmail.com>
2018-08-20 15:15:47 -0400

    experimental work to accomodate not having asDoubleArray() and getDouble() in every column

--- core/src/main/java/tech/tablesaw/conversion/TableConverter.java
+++ core/src/main/java/tech/tablesaw/conversion/TableConverter.java
@@ -1,15 +1,14 @@
 package tech.tablesaw.conversion;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import com.google.common.base.Preconditions;
-
 import smile.data.Attribute;
 import smile.data.AttributeDataset;
 import smile.data.NumericAttribute;
-import tech.tablesaw.columns.Column;
+import tech.tablesaw.api.NumericColumn;
 import tech.tablesaw.table.Relation;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class TableConverter {
 
@@ -20,75 +19,75 @@ public class TableConverter {
     }
 
     public double[][] doubleMatrix() {
-        return doubleMatrix(table.columns());
+        return doubleMatrix(table.numericColumns());
     }
 
     public double[][] doubleMatrix(int... columnIndicies) {
-        return doubleMatrix(table.columns(columnIndicies));
+        return doubleMatrix(table.numericColumns(columnIndicies));
     }
 
     public double[][] doubleMatrix(String... columnNames) {
-        return doubleMatrix(table.columns(columnNames));
+        return doubleMatrix(table.numericColumns(columnNames));
     }
 
     public float[][] floatMatrix() {
-        return floatMatrix(table.columns());
+        return floatMatrix(table.numericColumns());
     }
 
     public float[][] floatMatrix(int... columnIndicies) {
-        return floatMatrix(table.columns(columnIndicies));
+        return floatMatrix(table.numericColumns(columnIndicies));
     }
 
     public float[][] floatMatrix(String... columnNames) {
-        return floatMatrix(table.columns(columnNames));
+        return floatMatrix(table.numericColumns(columnNames));
     }
 
     public int[][] intMatrix() {
-        return intMatrix(table.columns());
+        return intMatrix(table.numericColumns());
     }
 
     public int[][] intMatrix(int... columnIndicies) {
-        return intMatrix(table.columns(columnIndicies));
+        return intMatrix(table.numericColumns(columnIndicies));
     }
 
     public int[][] intMatrix(String... columnNames) {
-        return intMatrix(table.columns(columnNames));
+        return intMatrix(table.numericColumns(columnNames));
     }
 
-    private static double[][] doubleMatrix(List<Column<?>> columns) {
-        Preconditions.checkArgument(columns.size() >= 1);
-        int obs = columns.get(0).size();
-        double[][] allVals = new double[obs][columns.size()];
+    private static double[][] doubleMatrix(List<NumericColumn<?>> numberColumns) {
+        Preconditions.checkArgument(numberColumns.size() >= 1);
+        int obs = numberColumns.get(0).size();
+        double[][] allVals = new double[obs][numberColumns.size()];
 
         for (int r = 0; r < obs; r++) {
-            for (int c = 0; c < columns.size(); c++) {
-                allVals[r][c] = columns.get(c).getDouble(r);
+            for (int c = 0; c < numberColumns.size(); c++) {
+                allVals[r][c] = numberColumns.get(c).getDouble(r);
             }
         }
         return allVals;
     }
 
-    private static float[][] floatMatrix(List<Column<?>> columns) {
-        Preconditions.checkArgument(columns.size() >= 1);
-        int obs = columns.get(0).size();
-        float[][] allVals = new float[obs][columns.size()];
+    private static float[][] floatMatrix(List<NumericColumn<?>> numberColumns) {
+        Preconditions.checkArgument(numberColumns.size() >= 1);
+        int obs = numberColumns.get(0).size();
+        float[][] allVals = new float[obs][numberColumns.size()];
 
         for (int r = 0; r < obs; r++) {
-            for (int c = 0; c < columns.size(); c++) {
-                allVals[r][c] = (float) columns.get(c).getDouble(r);
+            for (int c = 0; c < numberColumns.size(); c++) {
+                allVals[r][c] = (float) numberColumns.get(c).getDouble(r);
             }
         }
         return allVals;
     }
 
-    private static int[][] intMatrix(List<Column<?>> columns) {
-        Preconditions.checkArgument(columns.size() >= 1);
-        int obs = columns.get(0).size();
-        int[][] allVals = new int[obs][columns.size()];
+    private static int[][] intMatrix(List<NumericColumn<?>> numberColumns) {
+        Preconditions.checkArgument(numberColumns.size() >= 1);
+        int obs = numberColumns.get(0).size();
+        int[][] allVals = new int[obs][numberColumns.size()];
 
         for (int r = 0; r < obs; r++) {
-            for (int c = 0; c < columns.size(); c++) {
-                allVals[r][c] = (int) columns.get(c).getDouble(r);
+            for (int c = 0; c < numberColumns.size(); c++) {
+                allVals[r][c] = (int) numberColumns.get(c).getDouble(r);
             }
         }
         return allVals;
@@ -96,19 +95,19 @@ public class TableConverter {
 
     public AttributeDataset smileDataset(String responseColName) {
         return smileDataset(
-            table.column(responseColName),
-            table.columns().stream().filter(c -> !c.name().equals(responseColName)).collect(Collectors.toList()));
+            table.numberColumn(responseColName),
+            table.numericColumns().stream().filter(c -> !c.name().equals(responseColName)).collect(Collectors.toList()));
     }  
 
     public AttributeDataset smileDataset(int responseColIndex, int... variablesColIndices) {
-        return smileDataset(table.column(responseColIndex), table.columns(variablesColIndices));
+        return smileDataset(table.numberColumn(responseColIndex), table.numericColumns(variablesColIndices));
     }  
 
     public AttributeDataset smileDataset(String responseColName, String... variablesColNames) {
-        return smileDataset(table.column(responseColName), table.columns(variablesColNames));
+        return smileDataset(table.numberColumn(responseColName), table.numericColumns(variablesColNames));
     }
 
-    private AttributeDataset smileDataset(Column<?> responseCol, List<Column<?>> variableCols) {
+    private AttributeDataset smileDataset(NumericColumn<?> responseCol, List<NumericColumn<?>> variableCols) {
         AttributeDataset data = new AttributeDataset(table.name(),
             variableCols.stream().map(this::colToAttribute).toArray(Attribute[]::new),
             colToAttribute(responseCol));
@@ -121,10 +120,10 @@ public class TableConverter {
     }
 
     /**
-     * We convert all columns to NumericAttribute. Smile's AttributeDataset only stores data as double.
+     * We convert all numberColumns to NumericAttribute. Smile's AttributeDataset only stores data as double.
      * While Smile defines NominalAttribute and DateAttribute they appear to be little used.
      */
-    private Attribute colToAttribute(Column<?> col) {
+    private Attribute colToAttribute(NumericColumn<?> col) {
         return new NumericAttribute(col.name());
     }
 
