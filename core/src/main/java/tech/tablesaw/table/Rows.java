ab27d59c659efe20353be240212f44c722e376da
Larry White <ljw1001@gmail.com>
2018-08-20 09:18:59 -0400

    uncommented code

--- core/src/main/java/tech/tablesaw/table/Rows.java
+++ core/src/main/java/tech/tablesaw/table/Rows.java
@@ -36,8 +36,6 @@ public final class Rows {
      */
     @SuppressWarnings({"rawtypes","unchecked"})
     public static void copyRowsToTable(Selection rows, Table oldTable, Table newTable) {
-        //copyRowsToTable(rows.toArray(), oldTable, newTable);
-
         for (int columnIndex = 0; columnIndex < oldTable.columnCount(); columnIndex++) {
             Column oldColumn = oldTable.column(columnIndex);
             for (int i : rows) {
