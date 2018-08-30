3ed2b09d25afb97296d7c79cd98563f628eca88c
Larry White <ljw1001@gmail.com>
2018-08-30 13:28:40 -0400

    deleted unused class

--- core/src/main/java/tech/tablesaw/columns/numbers/IntegerIterable.java
+++
@@ -1,9 +0,0 @@
-package tech.tablesaw.columns.numbers;
-
-import it.unimi.dsi.fastutil.ints.IntIterator;
-
-public interface IntegerIterable extends Iterable<Integer> {
-
-    IntIterator intIterator();
-
-}
