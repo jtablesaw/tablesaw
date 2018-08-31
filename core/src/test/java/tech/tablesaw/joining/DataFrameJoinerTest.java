382816466cbbb516b5698b0db77802c6d78b8eef
Gregor Okorn <go12665@washgas.com>
2018-08-31 14:15:05 -0400

    Implement cleaner solution for multi-table join.

--- core/src/test/java/tech/tablesaw/joining/DataFrameJoinerTest.java
+++ core/src/test/java/tech/tablesaw/joining/DataFrameJoinerTest.java
@@ -79,16 +79,16 @@ public class DataFrameJoinerTest {
     private static Table createSTUDENT() {
         return Table.read().csv(
             "ID,FirstName,LastName,City,State,Age,USID,GradYear\n"
-                    + "1,Bob,Barney,Burke,VA,20,11122,2019\n"
-                    + "2,Chris,Cabello,Canyonville,OR,20,22224,2019\n"
-                    + "3,Dan,Dirble,Denver,CO,21,33335,2020\n"
-                    + "4,Edward,Earhardt,Easterly,WA,21,44339,2021\n"
-                    + "5,Frank,Farnsworth,Fredriksburg,VA,22,55338,2019\n"
-                    + "6,George,Gabral,Garrisburg,MD,22,66337,2020\n"
-                    + "7,Michael,Marbury,Milton,NH,23,77330,2020\n"
-                    + "8,Robert,Riley,Roseburg,OR,23,88836,2020\n"
-                    + "9,Bob,Earhardt,Milton,NH,24,93333,2019\n"
-                    + "10,Dan,Gabral,Easterly,WA,24,13333,2020\n",
+                    + "1,Bob,Barney,Burke,VA,17,11122,2019\n"
+                    + "2,Chris,Cabello,Canyonville,OR,17,22224,2019\n"
+                    + "3,Dan,Dirble,Denver,CO,17,33335,2020\n"
+                    + "4,Edward,Earhardt,Easterly,WA,18,44339,2021\n"
+                    + "5,Frank,Farnsworth,Fredriksburg,VA,18,55338,2019\n"
+                    + "6,George,Gabral,Garrisburg,MD,19,66337,2020\n"
+                    + "7,Michael,Marbury,Milton,NH,19,77330,2020\n"
+                    + "8,Robert,Riley,Roseburg,OR,19,88836,2020\n"
+                    + "9,Bob,Earhardt,Milton,NH,50,93333,2019\n"
+                    + "10,Dan,Gabral,Easterly,WA,20,13333,2020\n",
             "Student");
     }
 
@@ -97,14 +97,14 @@ public class DataFrameJoinerTest {
             "ID,First,Last,Title,City,State,Age,USID,GradYear\n"
                     + "1,Bob,Cabello,Prof,Burke,VA,20,11333,2019\n"
                     + "2,Chris,Barney,TA,Canyonville,OR,20,22334,2019\n"
-                    + "3,Dan,Earhardt,Instructor,Denver,CO,19,33335,2020\n"
+                    + "3,Dan,Earhardt,Instructor,Denver,CO,22,33335,2020\n"
                     + "4,Edward,Dirble,Prof,Easterly,WA,22,43339,2020\n"
-                    + "5,Farnsworth,Frank,Prof,Fredriksburg,VA,18,55338,2019\n"
-                    + "6,Gabral,George,TA,Garrisburg,MD,20,66337,2019\n"
-                    + "7,Robert,Marbury,TA,Msilton,NH,22,73330,2020\n"
-                    + "8,Michael,Riley,TA,Roseburg,OR,22,88336,2020\n"
-                    + "9,Bob,Riley,Prof,Milton,NH,19,99933,2020\n"
-                    + "10,Earhardt,Gabral,Prof,Easterly,WA,21,13333,2019\n",
+                    + "5,Farnsworth,Frank,Prof,Fredriksburg,VA,22,55338,2019\n"
+                    + "6,Gabral,George,TA,Garrisburg,MD,18,66337,2019\n"
+                    + "7,Robert,Marbury,TA,Msilton,NH,23,73330,2020\n"
+                    + "8,Michael,Riley,TA,Roseburg,OR,23,88336,2020\n"
+                    + "9,Bob,Riley,Prof,Milton,NH,50,99933,2020\n"
+                    + "10,Earhardt,Gabral,Prof,Easterly,WA,24,13333,2019\n",
             "Instructor");
     }
 
@@ -113,31 +113,31 @@ public class DataFrameJoinerTest {
         return Table.read().csv(
                 "ID,First,Last,Dept,City,State,Age\n"
                         + "1,John,Cabello,ComputerScience,Burke,VA,20\n"
-                        + "2,Samantha,Barney,Writing,Canyonville,OR,21\n"
-                        + "3,Mark,Earhardt,Mathematics,Denver,CO,22\n"
-                        + "4,Christie,Dirble,Architecture,Easterly,WA,23\n"
-                        + "5,Bhawesh,Frank,Psychology,Fredriksburg,VA,24\n"
-                        + "6,Robert,George,Sociology,Garrisburg,MD,25\n"
-                        + "7,George,Marbury,Physics,Msilton,NH,26\n"
-                        + "8,Zhongyu,Riley,Chemistry,Roseburg,OR,27\n"
-                        + "9,Laura,Riley,Economics,Milton,NH,28\n"
-                        + "10,Sally,Gabral,Marketing,Easterly,WA,29\n",
+                        + "2,Samantha,Barney,Writing,Canyonville,OR,18\n"
+                        + "3,Mark,Earhardt,Mathematics,Denver,CO,35\n"
+                        + "4,Christie,Dirble,Architecture,Easterly,WA,36\n"
+                        + "5,Bhawesh,Frank,Psychology,Fredriksburg,VA,37\n"
+                        + "6,Robert,George,Sociology,Garrisburg,MD,38\n"
+                        + "7,George,Marbury,Physics,Msilton,NH,39\n"
+                        + "8,Zhongyu,Riley,Chemistry,Roseburg,OR,40\n"
+                        + "9,Laura,Riley,Economics,Milton,NH,50\n"
+                        + "10,Sally,Gabral,Marketing,Easterly,WA,42\n",
                 "DepartmentHead");
     }
     
     private static Table createCLASS() {
         return Table.read().csv(
             "ID,ClassType,Name,Level,Description,StartDate,EndDate,Completed,Age\n"
-                    + "1001,Math,Calculus,101,Newton math,2018-09-20,2018-12-17,false,16\n"
-                    + "1002,Math,Calculus,102,Newton math,2019-01-06,2019-03-06,false,17\n"
+                    + "1001,Math,Calculus,101,Newton math,2018-09-20,2018-12-17,false,20\n"
+                    + "1002,Math,Calculus,102,Newton math,2019-01-06,2019-03-06,false,18\n"
                     + "1003,Math,Calculus,103,Newton math,2019-03-10,2019-06-17,false,18\n"
-                    + "1004,Writing,Composition,101,Writing papers,2018-09-20,2018-12-17,false,19\n"
-                    + "1005,Writing,Composition,102,Writing papers,2019-01-06,2019-03-07,false,20\n"
-                    + "1006,Software,Programming,101,Programming basics,2018-09-22,2018-12-15,false,21\n"
-                    + "1007,Software,Programming,102,Programming basics,2019-01-05,2019-03-07,false,22\n"
-                    + "1008,Economics,Microeconomics,101,Basic micro economics,2018-09-20,2018-12-17,false,23\n"
-                    + "1009,Economics,Microeconomics,102,Basic micro economics,2018-01-05,2019-03-07,false,24\n"
-                    + "1010,Literature,Shakespeare,101,Understanding Shakespeare,2018-09-20,2018-12-17,false,25\n",
+                    + "1004,Writing,Composition,101,Writing papers,2018-09-20,2018-12-17,false,23\n"
+                    + "1005,Writing,Composition,102,Writing papers,2019-01-06,2019-03-07,false,24\n"
+                    + "1006,Software,Programming,101,Programming basics,2018-09-22,2018-12-15,false,25\n"
+                    + "1007,Software,Programming,102,Programming basics,2019-01-05,2019-03-07,false,26\n"
+                    + "1008,Economics,Microeconomics,101,Basic micro economics,2018-09-20,2018-12-17,false,27\n"
+                    + "1009,Economics,Microeconomics,102,Basic micro economics,2018-01-05,2019-03-07,false,28\n"
+                    + "1010,Literature,Shakespeare,101,Understanding Shakespeare,2018-09-20,2018-12-17,false,50\n",
             "Class");
     }
     
@@ -263,9 +263,20 @@ public class DataFrameJoinerTest {
         assert(joined.columnNames().containsAll(Arrays.asList(new String[] {
                 "T2.ID", "T2.City", "T2.State", "T2.USID", "T2.GradYear"})));
         assertEquals(16, joined.columnCount());
-        assertEquals(14, joined.rowCount());
+        assertEquals(5, joined.rowCount());
     }    
     
+    @Test
+    public void innerJoinInstructorStudentOnAge() {
+        Table STUDENT = createSTUDENT();
+        Table INSTRUCTOR = createINSTRUCTOR();
+        Table joined = INSTRUCTOR.join("Age").inner(true, STUDENT);
+        assert(joined.columnNames().containsAll(Arrays.asList(new String[] {
+                "T2.ID", "T2.City", "T2.State", "T2.USID", "T2.GradYear"})));
+        assertEquals(16, joined.columnCount());
+        assertEquals(5, joined.rowCount());
+    }    
+  
     @Test
     public void innerJoinStudentInstructorClassOnAge() {
         Table STUDENT = createSTUDENT();
@@ -276,8 +287,9 @@ public class DataFrameJoinerTest {
                 "T2.ID", "T2.City", "T2.State", "T2.USID", "T2.GradYear",
         "T3.ID"})));
         assertEquals(24, joined.columnCount());
-        assertEquals(14, joined.rowCount());
-    }    
+        assertEquals(7, joined.rowCount());
+    }
+    
     
     @Test
     public void innerJoinStudentInstructorClassDeptHeadOnAge() {
@@ -290,6 +302,7 @@ public class DataFrameJoinerTest {
                 "T2.ID", "T2.City", "T2.State", "T2.USID", "T2.GradYear",
                 "T3.ID", "T4.ID", "T4.First", "T4.Last", "T4.City", "T4.State"})));
         assertEquals(30, joined.columnCount());
-        assertEquals(14, joined.rowCount());
+        assertEquals(7, joined.rowCount());
     }
+
 }
