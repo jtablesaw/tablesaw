ba8e4680022c0779736318ca194c6ef415475958
Ben McCann <322311+benmccann@users.noreply.github.com>
2019-01-09 18:50:25 -0800

    Rename methods to start with assert

--- core/src/test/java/tech/tablesaw/columns/times/TimeFillersTest.java
+++ core/src/test/java/tech/tablesaw/columns/times/TimeFillersTest.java
@@ -11,7 +11,7 @@ import static tech.tablesaw.columns.datetimes.fillers.TemporalRangeIterable.rang
 
 public class TimeFillersTest {
 
-    private void testValues(Iterable<LocalTime> times, LocalTime... expected) {
+    private void assertContentEquals(Iterable<LocalTime> times, LocalTime... expected) {
         int num = 0;
         for (LocalTime value : times) {
             assertEquals(expected[num], value);
@@ -22,7 +22,7 @@ public class TimeFillersTest {
 
     @Test
     public void testFromToBy() {
-        testValues(create("times", new LocalTime[5]).fillWith(range(LocalTime.of(12, 30), // hour, minute
+        assertContentEquals(create("times", new LocalTime[5]).fillWith(range(LocalTime.of(12, 30), // hour, minute
                 LocalTime.of(21, 30), 2, ChronoUnit.HOURS)), LocalTime.of(12, 30), // year, month, day, hour, minute
                 LocalTime.of(14, 30), LocalTime.of(16, 30), LocalTime.of(18, 30), LocalTime.of(20, 30));
     }
