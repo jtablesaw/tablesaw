193aa92cbc779686b629acf0cf3278905fbe6df9
Larry White <ljw1001@gmail.com>
2019-03-06 22:11:42 -0500

    fixed bug in Table method stepWithRows(). Added a test

--- core/src/test/java/tech/tablesaw/api/TableTest.java
+++ core/src/test/java/tech/tablesaw/api/TableTest.java
@@ -298,6 +298,35 @@ public class TableTest {
     }
 
     @Test
+    public void stepWithRows() throws Exception {
+        Table t = Table.read()
+                .csv(CsvReadOptions.builder("../data/bush.csv").minimizeColumnSizes(true))
+                .first(6);
+
+        final int sum1 = (int) t.shortColumn("approval").sum();
+
+        RowConsumer rowConsumer = new RowConsumer();
+        t.stepWithRows(rowConsumer, 3);
+        assertEquals(sum1, rowConsumer.getSum());
+
+    }
+    private static class RowConsumer implements Consumer<Row[]> {
+
+        int sum = 0;
+
+        public int getSum() {
+            return sum;
+        }
+
+        @Override
+        public void accept(Row[] rows) {
+            for (int i = 0; i < 3; i++) {
+                sum += rows[i].getShort("approval");
+            }
+        }
+    }
+
+    @Test
     public void testRollWithNrows2() throws Exception {
         Table t = Table.read().csv(CsvReadOptions.builder("../data/bush.csv").minimizeColumnSizes(true)).first(4);
         ShortColumn approval = t.shortColumn("approval");
