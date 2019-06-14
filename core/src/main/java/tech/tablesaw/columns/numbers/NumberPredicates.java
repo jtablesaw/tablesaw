3337b1107f87906072658e09c5b94ede3ce489f5
Ben McCann <322311+benmccann@users.noreply.github.com>
2019-06-14 15:53:40 -0700

    Allow using all NumberPredicates in set method (#532)

--- core/src/main/java/tech/tablesaw/columns/numbers/NumberPredicates.java
+++ core/src/main/java/tech/tablesaw/columns/numbers/NumberPredicates.java
@@ -14,10 +14,6 @@
 
 package tech.tablesaw.columns.numbers;
 
-import tech.tablesaw.columns.Column;
-import tech.tablesaw.filtering.predicates.DoubleBiPredicate;
-import tech.tablesaw.filtering.predicates.DoubleRangePredicate;
-
 import java.util.function.DoublePredicate;
 
 /**
@@ -25,7 +21,7 @@ import java.util.function.DoublePredicate;
  * <p>
  * TODO(lwhite): Ensure each returns false when handling missing values
  */
-public interface NumberPredicates extends Column<Double> {
+public interface NumberPredicates {
 
     DoublePredicate isZero = i -> i == 0.0;
 
@@ -35,33 +31,37 @@ public interface NumberPredicates extends Column<Double> {
 
     DoublePredicate isNonNegative = i -> i >= 0;
 
-    DoubleBiPredicate isGreaterThan = (valueToTest, valueToCompareAgainst) -> valueToTest > valueToCompareAgainst;
+    static DoublePredicate isGreaterThan(double valueToCompareAgainst) {
+      return valueToTest -> valueToTest > valueToCompareAgainst;
+    }
 
-    DoubleRangePredicate isBetweenExclusive = new DoubleRangePredicate() {
-        @Override
-        public boolean test(double valueToTest, double rangeStart, double rangeEnd) {
-            return valueToTest > rangeStart && valueToTest < rangeEnd;
-        }
+    static DoublePredicate isBetweenExclusive(double rangeStart, double rangeEnd) {
+      return valueToTest -> valueToTest > rangeStart && valueToTest < rangeEnd;
     };
 
-    DoubleRangePredicate isBetweenInclusive = new DoubleRangePredicate() {
-        @Override
-        public boolean test(double valueToTest, double rangeStart, double rangeEnd) {
-            return valueToTest >= rangeStart && valueToTest <= rangeEnd;
-        }
+    static DoublePredicate isBetweenInclusive(double rangeStart, double rangeEnd) {
+      return valueToTest -> valueToTest >= rangeStart && valueToTest <= rangeEnd;
     };
 
-    DoubleBiPredicate isGreaterThanOrEqualTo = (valueToTest, valueToCompareAgainst) -> valueToTest >=
-            valueToCompareAgainst;
+    static DoublePredicate isGreaterThanOrEqualTo(double valueToCompareAgainst) {
+      return valueToTest -> valueToTest >= valueToCompareAgainst;  
+    }
 
-    DoubleBiPredicate isLessThan = (valueToTest, valueToCompareAgainst) -> valueToTest < valueToCompareAgainst;
+    static DoublePredicate isLessThan(double valueToCompareAgainst) {
+      return valueToTest -> valueToTest < valueToCompareAgainst;
+    }
 
-    DoubleBiPredicate isLessThanOrEqualTo = (valueToTest, valueToCompareAgainst) -> valueToTest <=
-            valueToCompareAgainst;
+    static DoublePredicate isLessThanOrEqualTo(double valueToCompareAgainst) {
+       return valueToTest -> valueToTest <= valueToCompareAgainst;
+    }
 
-    DoubleBiPredicate isEqualTo = (valueToTest, valueToCompareAgainst) -> valueToTest == valueToCompareAgainst;
+    static DoublePredicate isEqualTo(double valueToCompareAgainst) {
+      return valueToTest -> valueToTest == valueToCompareAgainst;
+    }
 
-    DoubleBiPredicate isNotEqualTo = (valueToTest, valueToCompareAgainst) -> valueToTest != valueToCompareAgainst;
+    static DoublePredicate isNotEqualTo(double valueToCompareAgainst) {
+      return valueToTest -> valueToTest != valueToCompareAgainst;
+    }
 
     DoublePredicate isMissing = i -> i != i;
 
