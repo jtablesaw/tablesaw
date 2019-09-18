0711ed706ab07d95be35ecd7bd36cd69b40a55da
Ben McCann <322311+benmccann@users.noreply.github.com>
2019-09-18 10:03:17 -0700

    Fix generics warning

--- core/src/main/java/tech/tablesaw/columns/strings/LookupTableWrapper.java
+++ core/src/main/java/tech/tablesaw/columns/strings/LookupTableWrapper.java
@@ -73,7 +73,7 @@ public class LookupTableWrapper {
     }
   }
 
-  public Class dictionaryClass() {
+  public Class<? extends DictionaryMap> dictionaryClass() {
     return dictionaryMap.getClass();
   }
 
