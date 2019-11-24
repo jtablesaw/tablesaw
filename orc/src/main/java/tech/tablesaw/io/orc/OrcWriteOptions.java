ecc83c21223990ee647b009008500f055f8c2414
murtuza-ranapur <mranapurwala56@gmail.com>
2019-11-24 12:04:20 +0530

    Reformatted code

--- orc/src/main/java/tech/tablesaw/io/orc/OrcWriteOptions.java
+++ orc/src/main/java/tech/tablesaw/io/orc/OrcWriteOptions.java
@@ -1,55 +1,58 @@
 package tech.tablesaw.io.orc;
 
+import java.io.File;
+import java.io.IOException;
 import org.apache.orc.OrcFile;
 import tech.tablesaw.io.Destination;
 import tech.tablesaw.io.WriteOptions;
 
-import java.io.File;
-import java.io.IOException;
-
 public class OrcWriteOptions extends WriteOptions {
 
+  private OrcFile.WriterOptions writerOptions;
+  private File outputPath;
+
+  protected OrcWriteOptions(Builder builder) {
+    super(builder);
+    writerOptions = builder.writerOptions;
+    outputPath = builder.outputFile;
+  }
+
+  public OrcFile.WriterOptions getWriterOptions() {
+    return writerOptions;
+  }
+
+  public File getOutputPath() {
+    return outputPath;
+  }
+
+  public static Builder builder(File file) throws IOException {
+    return new Builder(file);
+  }
+
+  public static Builder builder(Destination destination) {
+    return new Builder(destination);
+  }
+
+  static class Builder extends WriteOptions.Builder {
     private OrcFile.WriterOptions writerOptions;
-    private File outputPath;
+    private File outputFile;
 
-    protected OrcWriteOptions(Builder builder) {
-        super(builder);
-        writerOptions = builder.writerOptions;
-        outputPath = builder.outputFile;
+    protected Builder(Destination dest) {
+      super(dest);
     }
 
-    public OrcFile.WriterOptions getWriterOptions() {
-        return writerOptions;
+    protected Builder(File file) throws IOException {
+      super(file);
+      outputFile = file;
     }
 
-    public File getOutputPath() {
-        return outputPath;
+    public Builder ocrWriteOptions(OrcFile.WriterOptions writerOptions) {
+      this.writerOptions = writerOptions;
+      return this;
     }
 
-    public static Builder builder(File file) throws IOException {
-        return new Builder(file);
+    public OrcWriteOptions build() {
+      return new OrcWriteOptions(this);
     }
-
-    public static Builder builder(Destination destination) {
-        return new Builder(destination);
-    }
-
-    static class Builder extends WriteOptions.Builder{
-        private OrcFile.WriterOptions writerOptions;
-        private File outputFile;
-        protected Builder(Destination dest) {
-            super(dest);
-        }
-        protected Builder(File file) throws IOException {
-            super(file);
-            outputFile = file;
-        }
-        public Builder ocrWriteOptions(OrcFile.WriterOptions writerOptions) {
-            this.writerOptions = writerOptions;
-            return this;
-        }
-        public OrcWriteOptions build() {
-            return new OrcWriteOptions(this);
-        }
-    }
+  }
 }
