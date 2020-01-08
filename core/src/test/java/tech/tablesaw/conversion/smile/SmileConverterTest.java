37f9087a0cdeed0cc3db58bc3fc9a42b8131d09b
Ben McCann <322311+benmccann@users.noreply.github.com>
2020-01-08 09:24:39 -0800

    Upgrade to Smile 2.0.0 (#735)

--- core/src/test/java/tech/tablesaw/conversion/smile/SmileConverterTest.java
+++ core/src/test/java/tech/tablesaw/conversion/smile/SmileConverterTest.java
@@ -14,17 +14,33 @@
 
 package tech.tablesaw.conversion.smile;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
 import org.junit.jupiter.api.Test;
-import smile.classification.RandomForest;
-import smile.data.AttributeDataset;
+import smile.data.DataFrame;
+import smile.data.formula.Formula;
+import smile.regression.LinearModel;
 import smile.regression.OLS;
+import tech.tablesaw.api.BooleanColumn;
+import tech.tablesaw.api.DateColumn;
+import tech.tablesaw.api.DateTimeColumn;
+import tech.tablesaw.api.DoubleColumn;
+import tech.tablesaw.api.FloatColumn;
+import tech.tablesaw.api.InstantColumn;
+import tech.tablesaw.api.IntColumn;
+import tech.tablesaw.api.LongColumn;
+import tech.tablesaw.api.ShortColumn;
+import tech.tablesaw.api.StringColumn;
 import tech.tablesaw.api.Table;
+import tech.tablesaw.api.TextColumn;
+import tech.tablesaw.api.TimeColumn;
 
 public class SmileConverterTest {
 
@@ -33,50 +49,43 @@ public class SmileConverterTest {
     Table moneyball = Table.read().csv("../data/baseball.csv");
     moneyball.addColumns(
         moneyball.numberColumn("RS").subtract(moneyball.numberColumn("RA")).setName("RD"));
-    OLS winsModel = new OLS(moneyball.select("W", "RD").smile().numericDataset("RD"));
+
+    LinearModel winsModel =
+        OLS.fit(Formula.lhs("RD"), moneyball.select("W", "RD").smile().toDataFrame());
     assertNotNull(winsModel.toString());
   }
 
   @Test
-  public void regressionWithStratifiedSampleTest() throws IOException {
-    Table moneyball = Table.read().csv("../data/baseball.csv");
-    Table[] splits = moneyball.stratifiedSampleSplit(moneyball.stringColumn("Team"), 0.6);
-    Table stratifiedMoneyBall = splits[0];
-    stratifiedMoneyBall.addColumns(
-        stratifiedMoneyBall
-            .numberColumn("RS")
-            .subtract(stratifiedMoneyBall.numberColumn("RA"))
-            .setName("RD"));
-    OLS winsModel = new OLS(stratifiedMoneyBall.select("W", "RD").smile().numericDataset("RD"));
-    assertNotNull(winsModel.toString());
-  }
-
-  @Test
-  public void classification() throws IOException {
-    Table moneyball = Table.read().csv("../data/baseball.csv");
-    RandomForest playoffsModel =
-        new RandomForest(moneyball.smile().nominalDataset("Playoffs", "RS", "RA", "OBP"), 1);
-    assertNotNull(playoffsModel.toString());
-  }
-
-  @Test
-  public void nominalDatasetToString() throws IOException {
-    Table moneyball = Table.read().csv("../data/baseball.csv");
-    AttributeDataset dataset =
-        moneyball.smile().nominalDataset("Playoffs", "League", "RS", "RA", "OBP");
-    assertNotNull(dataset.toString());
-  }
-
-  @Test
-  public void columnNames() throws IOException {
-    Table moneyball = Table.read().csv("../data/baseball.csv");
-    String responseColumnName = "Playoffs";
-    String[] variableColumnNames = new String[] {"League", "RS", "RA", "OBP"};
-    AttributeDataset dataset =
-        moneyball.smile().nominalDataset(responseColumnName, variableColumnNames);
-    String[] resultNames =
-        Arrays.stream(dataset.attributes()).map(attr -> attr.getName()).toArray(String[]::new);
-    assertEquals(responseColumnName, dataset.responseAttribute().getName());
-    assertArrayEquals(variableColumnNames, resultNames);
+  public void allColumnTypes() throws IOException {
+    Table table = Table.create();
+    table.addColumns(BooleanColumn.create("boolean", new boolean[] {true, false}));
+    table.addColumns(DoubleColumn.create("double", new double[] {1.2, 3.4}));
+    table.addColumns(FloatColumn.create("float", new float[] {5.6f, 7.8f}));
+    table.addColumns(
+        InstantColumn.create(
+            "instant",
+            new Instant[] {
+              Instant.ofEpochMilli(1578452479123l), Instant.ofEpochMilli(1578451111111l)
+            }));
+    table.addColumns(IntColumn.create("int", new int[] {8, 9}));
+    table.addColumns(
+        DateColumn.create(
+            "date", new LocalDate[] {LocalDate.of(2020, 01, 01), LocalDate.of(2020, 01, 07)}));
+    table.addColumns(
+        DateTimeColumn.create(
+            "datetime",
+            new LocalDateTime[] {
+              LocalDateTime.ofInstant(Instant.ofEpochMilli(1333352479123l), ZoneOffset.UTC),
+              LocalDateTime.ofInstant(Instant.ofEpochMilli(1333333333333l), ZoneOffset.UTC)
+            }));
+    table.addColumns(
+        TimeColumn.create(
+            "time", new LocalTime[] {LocalTime.of(8, 37, 48), LocalTime.of(8, 59, 06)}));
+    table.addColumns(LongColumn.create("long", new long[] {3l, 4l}));
+    table.addColumns(ShortColumn.create("short", new short[] {1, 2}));
+    table.addColumns(StringColumn.create("string", new String[] {"james", "bond"}));
+    table.addColumns(TextColumn.create("text", new String[] {"foo", "bar"}));
+    DataFrame dataframe = table.smile().toDataFrame();
+    assertEquals(2, dataframe.nrows());
   }
 }
