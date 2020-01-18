57ee43fa8657378734ea705ad701ca4828d36415
jackie-h <10471950+jackie-h@users.noreply.github.com>
2020-01-18 11:24:35 -0500

    Clean up test - use constructors

--- core/src/test/java/tech/tablesaw/table/TableTransposeTest.java
+++ core/src/test/java/tech/tablesaw/table/TableTransposeTest.java
@@ -12,26 +12,24 @@ import tech.tablesaw.api.IntColumn;
 import tech.tablesaw.api.LongColumn;
 import tech.tablesaw.api.StringColumn;
 import tech.tablesaw.api.Table;
+import tech.tablesaw.columns.numbers.DoubleColumnType;
 
 public class TableTransposeTest {
 
   @Test
   void transposeDoubles() {
-    StringColumn label = StringColumn.create("label").append("row1").append("row2").append("row3");
-    DoubleColumn value = DoubleColumn.create("value1").append(1.0).append(1.1).append(1.2);
-    DoubleColumn value2 = DoubleColumn.create("value2").append(2.0).append(2.1).append(2.2);
+    StringColumn label = StringColumn.create("label", new String[]{ "row1", "row2", "row3"});
+    DoubleColumn value = DoubleColumn.create("value1", new double[]{ 1.0, 1.1, 1.2 });
+    DoubleColumn value2 = DoubleColumn.create("value2", new double[]{ 2.0, 2.1, 2.2});
 
-    Table testTable = Table.create("Data");
-    testTable.addColumns(label);
-    testTable.addColumns(value);
-    testTable.addColumns(value2);
+    Table testTable = Table.create("Data", label, value, value2);
     Table result = testTable.transpose();
 
     assertTableContents(
         new Object[][] {
-          {"label", "row1", "row2", "row3"},
-          {"value1", 1.0, 1.1, 1.2},
-          {"value2", 2.0, 2.1, 2.2},
+            {"label", "row1", "row2", "row3"},
+            {"value1", 1.0, 1.1, 1.2},
+            {"value2", 2.0, 2.1, 2.2},
         },
         result);
     assertEquals(testTable.name(), result.name());
@@ -40,21 +38,18 @@ public class TableTransposeTest {
 
   @Test
   void transposeWithMissingData() {
-    StringColumn label = StringColumn.create("label").append("row1").append("row2").append("row3");
-    DoubleColumn value = DoubleColumn.create("value1").append(1.0).appendMissing().append(1.2);
-    DoubleColumn value2 = DoubleColumn.create("value2").append(2.0).append(2.1).append(2.2);
+    StringColumn label = StringColumn.create("label", new String[]{ "row1", "row2", "row3"});
+    DoubleColumn value = DoubleColumn.create("value1", new double[] { 1.0, DoubleColumnType.missingValueIndicator(), 1.2 });
+    DoubleColumn value2 = DoubleColumn.create("value2", new double[] { 2.0, 2.1, 2.2 });
 
-    Table testTable = Table.create("Data");
-    testTable.addColumns(label);
-    testTable.addColumns(value);
-    testTable.addColumns(value2);
+    Table testTable = Table.create("Data", label, value, value2);
     Table result = testTable.transpose();
 
     assertTableContents(
         new Object[][] {
-          {"label", "row1", "row2", "row3"},
-          {"value1", 1.0, Double.NaN, 1.2},
-          {"value2", 2.0, 2.1, 2.2},
+            {"label", "row1", "row2", "row3"},
+            {"value1", 1.0, Double.NaN, 1.2},
+            {"value2", 2.0, 2.1, 2.2},
         },
         result);
 
@@ -63,35 +58,29 @@ public class TableTransposeTest {
 
   @Test
   void transposeFloats() {
-    StringColumn label = StringColumn.create("label").append("row1").append("row2").append("row3");
-    FloatColumn value = FloatColumn.create("value1").append(1.0f).append(1.1f).append(1.2f);
-    FloatColumn value2 = FloatColumn.create("value2").append(2.0f).append(2.1f).append(2.2f);
+    StringColumn label = StringColumn.create("label", new String[]{ "row1", "row2", "row3"});
+    FloatColumn value = FloatColumn.create("value1", new float[] { 1.0f, 1.1f, 1.2f });
+    FloatColumn value2 = FloatColumn.create("value2", new float[] { 2.0f, 2.1f, 2.2f});
 
-    Table testTable = Table.create("Data");
-    testTable.addColumns(label);
-    testTable.addColumns(value);
-    testTable.addColumns(value2);
+    Table testTable = Table.create("Data", label, value, value2);
     Table result = testTable.transpose();
     assertEquals(testTable.print(), result.transpose().print());
   }
 
   @Test
   void transposeIntegers() {
-    StringColumn label = StringColumn.create("label").append("row1").append("row2").append("row3");
-    IntColumn value = IntColumn.create("value1").append(1).append(2).append(3);
-    IntColumn value2 = IntColumn.create("value2").append(4).append(5).append(6);
+    StringColumn label = StringColumn.create("label", new String[]{ "row1", "row2", "row3"});
+    IntColumn value = IntColumn.create("value1", new int[]{ 1, 2, 3 });
+    IntColumn value2 = IntColumn.create("value2", new int[]{ 4, 5, 6 });
 
-    Table testTable = Table.create("Data");
-    testTable.addColumns(label);
-    testTable.addColumns(value);
-    testTable.addColumns(value2);
+    Table testTable = Table.create("Data", label, value, value2);
     Table result = testTable.transpose();
 
     assertTableContents(
         new Object[][] {
-          {"label", "row1", "row2", "row3"},
-          {"value1", 1, 2, 3},
-          {"value2", 4, 5, 6}
+            {"label", "row1", "row2", "row3"},
+            {"value1", 1, 2, 3},
+            {"value2", 4, 5, 6}
         },
         result);
 
@@ -100,21 +89,18 @@ public class TableTransposeTest {
 
   @Test
   void transposeLongs() {
-    StringColumn label = StringColumn.create("label").append("row1").append("row2").append("row3");
-    LongColumn value = LongColumn.create("value1").append(1).append(2).append(3);
-    LongColumn value2 = LongColumn.create("value2").append(4).append(5).append(6);
+    StringColumn label = StringColumn.create("label", new String[]{ "row1", "row2", "row3"});
+    LongColumn value = LongColumn.create("value1", new long[]{ 1, 2, 3 });
+    LongColumn value2 = LongColumn.create("value2", new long[] { 4, 5, 6});
 
-    Table testTable = Table.create("Data");
-    testTable.addColumns(label);
-    testTable.addColumns(value);
-    testTable.addColumns(value2);
+    Table testTable = Table.create("Data", label, value, value2);
     Table result = testTable.transpose();
 
     assertTableContents(
         new Object[][] {
-          {"label", "row1", "row2", "row3"},
-          {"value1", 1L, 2L, 3L},
-          {"value2", 4L, 5L, 6L}
+            {"label", "row1", "row2", "row3"},
+            {"value1", 1L, 2L, 3L},
+            {"value2", 4L, 5L, 6L}
         },
         result);
 
@@ -123,21 +109,18 @@ public class TableTransposeTest {
 
   @Test
   void transposeBooleans() {
-    StringColumn label = StringColumn.create("label").append("row1").append("row2").append("row3");
-    BooleanColumn value = BooleanColumn.create("value1").append(true).append(true).append(true);
-    BooleanColumn value2 = BooleanColumn.create("value2").append(false).append(false).append(false);
+    StringColumn label = StringColumn.create("label", new String[]{ "row1", "row2", "row3"});
+    BooleanColumn value = BooleanColumn.create("value1", new boolean[]{ true, true, true });
+    BooleanColumn value2 = BooleanColumn.create("value2", new boolean[]{ false, false, false});
 
-    Table testTable = Table.create("Data");
-    testTable.addColumns(label);
-    testTable.addColumns(value);
-    testTable.addColumns(value2);
+    Table testTable = Table.create("Data", label, value, value2);
     Table result = testTable.transpose();
 
     assertTableContents(
         new Object[][] {
-          {"label", "row1", "row2", "row3"},
-          {"value1", true, true, true},
-          {"value2", false, false, false}
+            {"label", "row1", "row2", "row3"},
+            {"value1", true, true, true},
+            {"value2", false, false, false}
         },
         result);
 
@@ -146,23 +129,18 @@ public class TableTransposeTest {
 
   @Test
   void transposeStrings() {
-    StringColumn label = StringColumn.create("label").append("row1").append("row2").append("row3");
-    StringColumn value =
-        StringColumn.create("fruit").append("apple").append("banana").append("pear");
-    StringColumn value2 =
-        StringColumn.create("colour").append("red").append("yellow").append("green");
+    StringColumn label = StringColumn.create("label", new String[]{ "row1", "row2", "row3"});
+    StringColumn value = StringColumn.create("fruit", new String[]{"apple", "banana","pear"});
+    StringColumn value2 = StringColumn.create("colour", new String[]{"red", "yellow", "green"});
 
-    Table testTable = Table.create("Data");
-    testTable.addColumns(label);
-    testTable.addColumns(value);
-    testTable.addColumns(value2);
+    Table testTable = Table.create("Data", label, value, value2);
     Table result = testTable.transpose();
 
     assertTableContents(
         new Object[][] {
-          {"label", "row1", "row2", "row3"},
-          {"fruit", "apple", "banana", "pear"},
-          {"colour", "red", "yellow", "green"}
+            {"label", "row1", "row2", "row3"},
+            {"fruit", "apple", "banana", "pear"},
+            {"colour", "red", "yellow", "green"}
         },
         result);
 
@@ -171,15 +149,11 @@ public class TableTransposeTest {
 
   @Test
   void transposeMixedTypes() {
-    StringColumn label = StringColumn.create("label").append("row1").append("row2").append("row3");
-    DoubleColumn value = DoubleColumn.create("value1").append(1.0).append(1.1).append(1.2);
-    StringColumn value2 =
-        StringColumn.create("colour").append("red").append("yellow").append("green");
+    StringColumn label = StringColumn.create("label", new String[]{ "row1", "row2", "row3"});
+    DoubleColumn value = DoubleColumn.create("value1", new double[] { 1.0, 1.1, 1.2 });
+    StringColumn value2 = StringColumn.create("colour", new String[]{ "red", "yellow", "green" });
 
-    Table testTable = Table.create("Data");
-    testTable.addColumns(label);
-    testTable.addColumns(value);
-    testTable.addColumns(value2);
+    Table testTable = Table.create("Data", label, value, value2);
 
     try {
       testTable.transpose();
