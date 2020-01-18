f0ac91b6942890a4a8f6585dcc5f0fbc99194801
jackie-h <10471950+jackie-h@users.noreply.github.com>
2020-01-18 15:23:49 -0500

    Add tests for more data types, fix boolean column to merge

--- core/src/test/java/tech/tablesaw/table/TableSummaryTest.java
+++ core/src/test/java/tech/tablesaw/table/TableSummaryTest.java
@@ -2,20 +2,21 @@ package tech.tablesaw.table;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
+import tech.tablesaw.api.BooleanColumn;
+import tech.tablesaw.api.DateColumn;
 import tech.tablesaw.api.DoubleColumn;
 import tech.tablesaw.api.StringColumn;
 import tech.tablesaw.api.Table;
 
 public class TableSummaryTest {
   @Test
-  public void summaryDoublesOnly() {
-    DoubleColumn value = DoubleColumn.create("value1").append(1.0).append(1.1).append(1.2);
-    DoubleColumn value2 = DoubleColumn.create("value2").append(2.0).append(2.1).append(2.2);
+  public void summaryTestTwoDoubleColumnsStatistics() {
+    DoubleColumn value = DoubleColumn.create("value1", new double[]{1.0, 1.1, 1.2});
+    DoubleColumn value2 = DoubleColumn.create("value2", new double[]{2.0, 2.1, 2.2});
 
-    Table testTable = Table.create("Data");
-    testTable.addColumns(value);
-    testTable.addColumns(value2);
+    Table testTable = Table.create("Data", value, value2);
     Table result = testTable.summary();
     assertEquals(
         "                            Data                             \n"
@@ -34,33 +35,37 @@ public class TableSummaryTest {
 
   @Test
   public void summaryMixedTypes() {
-    StringColumn label = StringColumn.create("label").append("row1").append("row2").append("row3");
-    DoubleColumn value = DoubleColumn.create("value1").append(1.0).append(1.1).append(1.2);
-    StringColumn label2 =
-        StringColumn.create("label2").append("row1").append("row5").append("row6");
-    DoubleColumn value2 = DoubleColumn.create("value2").append(2.0).append(2.1).append(2.2);
+    StringColumn label =
+        StringColumn.create("label", new String[]{"yellow", "yellow", "green"});
+    DoubleColumn value = DoubleColumn.create("value1", new double[]{ 1.0, 1.1, 1.2});
+    BooleanColumn booleanValue = BooleanColumn.create("truthy", new boolean[]{ true, false, true});
+    DateColumn dateColumn = DateColumn.create("dates", new LocalDate[]{
+        LocalDate.of(2001, 1, 1),
+        LocalDate.of(2002, 1, 1),
+        LocalDate.of(2001, 1, 1) });
 
-    Table testTable = Table.create("Data");
-    testTable.addColumns(label);
-    testTable.addColumns(value);
-    testTable.addColumns(label2);
-    testTable.addColumns(value2);
+    Table testTable = Table.create("Data", label, value, booleanValue, dateColumn);
     Table result = testTable.summary();
     assertEquals(
-        "                                       Data                                        \n"
-            + "  Summary   |  label  |         value1         |  label2  |        value2         |\n"
-            + "-----------------------------------------------------------------------------------\n"
-            + "     Count  |      3  |                     3  |       3  |                    3  |\n"
-            + "    Unique  |      3  |                        |       3  |                       |\n"
-            + "       Top  |   row1  |                        |    row1  |                       |\n"
-            + " Top Freq.  |      1  |                        |       1  |                       |\n"
-            + "       sum  |         |                   3.3  |          |                  6.3  |\n"
-            + "      Mean  |         |                   1.1  |          |                  2.1  |\n"
-            + "       Min  |         |                     1  |          |                    2  |\n"
-            + "       Max  |         |                   1.2  |          |                  2.2  |\n"
-            + "     Range  |         |   0.19999999999999996  |          |  0.20000000000000018  |\n"
-            + "  Variance  |         |  0.009999999999999995  |          |  0.01000000000000004  |\n"
-            + "  Std. Dev  |         |   0.09999999999999998  |          |   0.1000000000000002  |",
+        "                                   Data                                    \n"
+            + "  Summary   |  label   |         value1         |  truthy  |    dates     |\n"
+            + "---------------------------------------------------------------------------\n"
+            + "     Count  |       3  |                     3  |          |           3  |\n"
+            + "    Unique  |       2  |                        |          |              |\n"
+            + "       Top  |  yellow  |                        |          |              |\n"
+            + " Top Freq.  |       2  |                        |          |              |\n"
+            + "       sum  |          |                   3.3  |          |              |\n"
+            + "      Mean  |          |                   1.1  |          |              |\n"
+            + "       Min  |          |                     1  |          |              |\n"
+            + "       Max  |          |                   1.2  |          |              |\n"
+            + "     Range  |          |   0.19999999999999996  |          |              |\n"
+            + "  Variance  |          |  0.009999999999999995  |          |              |\n"
+            + "  Std. Dev  |          |   0.09999999999999998  |          |              |\n"
+            + "     false  |          |                        |       1  |              |\n"
+            + "      true  |          |                        |       2  |              |\n"
+            + "   Missing  |          |                        |          |           0  |\n"
+            + "  Earliest  |          |                        |          |  2001-01-01  |\n"
+            + "    Latest  |          |                        |          |  2002-01-01  |",
         result.print());
   }
 }
