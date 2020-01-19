226232fca42bf18924f84073acd1ea3fcb595466
jackie-h <10471950+jackie-h@users.noreply.github.com>
2020-01-19 10:53:12 -0500

    move useful helper into class for reuse in other tests

--- core/src/test/java/tech/tablesaw/SortTest.java
+++ core/src/test/java/tech/tablesaw/SortTest.java
@@ -16,6 +16,7 @@ package tech.tablesaw;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static tech.tablesaw.TableAssertions.assertTablesEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
@@ -147,27 +148,5 @@ public class SortTest {
     Throwable thrown = assertThrows(IllegalStateException.class, () -> Sort.create(table, ">col2"));
 
     assertEquals("Unrecognized Column: '>col2'", thrown.getMessage());
-  }
-
-  /**
-   * Make sure each row in each table match
-   *
-   * @param compareWith the table that was sorted using some external means e.g. excel. i.e known
-   *     good data
-   * @param sortedTable the table that was sorted with Tablesaw
-   */
-  private void assertTablesEquals(Table compareWith, Table sortedTable) {
-    assertEquals(
-        sortedTable.rowCount(), compareWith.rowCount(), "both tables have the same number of rows");
-    int maxRows = sortedTable.rowCount();
-    int numberOfColumns = sortedTable.columnCount();
-    for (int rowIndex = 0; rowIndex < maxRows; rowIndex++) {
-      for (int columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
-        assertEquals(
-            sortedTable.get(rowIndex, columnIndex),
-            compareWith.get(rowIndex, columnIndex),
-            "cells[" + rowIndex + ", " + columnIndex + "] do not match");
-      }
-    }
   }
 }
