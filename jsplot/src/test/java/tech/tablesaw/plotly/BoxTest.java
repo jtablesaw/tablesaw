1cb6f40c72e81271dc48e1d821a027d4ca357f79
Larry White <lwhite1@users.noreply.github.com>
2020-02-21 14:24:21 -0500

    Issue 740 (missing override method caused incorrect return type) (#763)
    * fix for issue 740
    
    BoxBuilder does not override name(string) as it should, and the return value of the inherited method is incorrect.
    
    * added simple test
    
    * adding some nominal assertials to make codacy happy
    
    The codacy issues have been here forever, but are being flagged as new.

--- jsplot/src/test/java/tech/tablesaw/plotly/BoxTest.java
+++ jsplot/src/test/java/tech/tablesaw/plotly/BoxTest.java
@@ -1,12 +1,14 @@
 package tech.tablesaw.plotly;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import tech.tablesaw.plotly.components.Figure;
 import tech.tablesaw.plotly.traces.BoxTrace;
 
 @Disabled
-public class BoxTest {
+class BoxTest {
 
   private final Object[] x = {
     "sheep",
@@ -25,18 +27,23 @@ public class BoxTest {
   private final double[] y = {1, 4, 9, 16, 3, 6, 8, 8, 2, 4, 7, 11};
 
   @Test
-  public void testAsJavascript() {
+  void testAsJavascript() {
     BoxTrace trace = BoxTrace.builder(x, y).build();
-
-    System.out.println(trace.asJavascript(1));
+    assertNotNull(trace.asJavascript(1));
   }
 
   @Test
-  public void show() {
-
+  void show() {
     BoxTrace trace = BoxTrace.builder(x, y).build();
-
     Figure figure = new Figure(trace);
+    assertNotNull(figure);
     Plot.show(figure, "target");
+  }
+
+  /** Test ensures that the name() method returns a BoxTraceBuilder as expected. */
+  @Test
+  void name() {
+    BoxTrace trace = BoxTrace.builder(x, y).name("my name").build();
+    assertNotNull(trace);
   }
 }
