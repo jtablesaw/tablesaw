3516799663aab7b1968107c39ea6f8aade08cd91
Larry White <ljw1001@gmail.com>
2021-10-15 17:29:15 -0400

    add javadoc for SkipColumnType

--- core/src/main/java/tech/tablesaw/columns/SkipColumnType.java
+++ core/src/main/java/tech/tablesaw/columns/SkipColumnType.java
@@ -2,6 +2,10 @@ package tech.tablesaw.columns;
 
 import tech.tablesaw.io.ReadOptions;
 
+/**
+ * A special {@link tech.tablesaw.api.ColumnType} that can be used in a ColumnType[] for table
+ * imports to instruct the system to skip (i.e. don't import) a column
+ */
 public class SkipColumnType extends AbstractColumnType {
 
   private static SkipColumnType INSTANCE;
@@ -10,6 +14,7 @@ public class SkipColumnType extends AbstractColumnType {
     super(byteSize, name, printerFriendlyName);
   }
 
+  /** Returns the singleton instance of this class */
   public static SkipColumnType instance() {
     if (INSTANCE == null) {
       INSTANCE = new SkipColumnType(0, "SKIP", "Skipped");
@@ -17,18 +22,21 @@ public class SkipColumnType extends AbstractColumnType {
     return INSTANCE;
   }
 
+  /** {@inheritDoc} */
   @Override
   public Column<Void> create(String name) {
     throw new UnsupportedOperationException(
         "Column type " + name() + " doesn't support column creation");
   }
 
+  /** {@inheritDoc} */
   @Override
   public AbstractColumnParser<?> customParser(ReadOptions options) {
     throw new UnsupportedOperationException("Column type " + name() + " doesn't support parsing");
   }
 
-  public static Object missingValueIndicator() {
+  /** This method is not supported. It will always throw {@link UnsupportedOperationException} */
+  public static Object missingValueIndicator() throws UnsupportedOperationException {
     throw new UnsupportedOperationException(
         "Column type " + SKIP + " doesn't support missing values");
   }
