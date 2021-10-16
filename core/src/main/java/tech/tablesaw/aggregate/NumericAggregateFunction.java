f55195802939d34f1b9eed4cd67a13a1cbc3e491
Larry White <ljw1001@gmail.com>
2021-10-16 11:21:33 -0400

    Completed JavaDoc for NumericAggregateFunction and TimeAggregateFunction

--- core/src/main/java/tech/tablesaw/aggregate/NumericAggregateFunction.java
+++ core/src/main/java/tech/tablesaw/aggregate/NumericAggregateFunction.java
@@ -6,10 +6,15 @@ import tech.tablesaw.api.NumericColumn;
 /** A partial implementation of aggregate functions to summarize over a numeric column */
 public abstract class NumericAggregateFunction extends AggregateFunction<NumericColumn<?>, Double> {
 
+  /**
+   * Constructs a NumericAggregateFunction with the given name. The name is used as a column name in
+   * the output
+   */
   public NumericAggregateFunction(String name) {
     super(name);
   }
 
+  /** {@inheritDoc} */
   @Override
   public boolean isCompatibleColumn(ColumnType type) {
     return type.equals(ColumnType.DOUBLE)
@@ -19,6 +24,7 @@ public abstract class NumericAggregateFunction extends AggregateFunction<Numeric
         || type.equals(ColumnType.LONG);
   }
 
+  /** {@inheritDoc} */
   @Override
   public ColumnType returnType() {
     return ColumnType.DOUBLE;
--- core/src/main/java/tech/tablesaw/aggregate/TimeAggregateFunction.java
+++ core/src/main/java/tech/tablesaw/aggregate/TimeAggregateFunction.java
@@ -7,17 +7,24 @@ import tech.tablesaw.api.TimeColumn;
 /** A partial implementation of aggregate functions to summarize over a time column */
 public abstract class TimeAggregateFunction extends AggregateFunction<TimeColumn, LocalTime> {
 
+  /**
+   * Constructs a TimeAggregateFunction with the given name. The name is used as a column name in
+   * the output
+   */
   public TimeAggregateFunction(String name) {
     super(name);
   }
 
+  /** Returns a LocalTime object that is the result of applying this function to the given Column */
   public abstract LocalTime summarize(TimeColumn column);
 
+  /** {@inheritDoc} */
   @Override
   public boolean isCompatibleColumn(ColumnType type) {
     return type.equals(ColumnType.LOCAL_TIME);
   }
 
+  /** {@inheritDoc} */
   @Override
   public ColumnType returnType() {
     return ColumnType.LOCAL_TIME;
