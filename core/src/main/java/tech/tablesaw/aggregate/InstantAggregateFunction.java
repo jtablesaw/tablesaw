a3ad5d66bd1a8db4c6b5097f66e7d3418c653611
Larry White <ljw1001@gmail.com>
2021-10-16 11:23:58 -0400

    Completed JavaDoc for InstantAggregateFunction

--- core/src/main/java/tech/tablesaw/aggregate/InstantAggregateFunction.java
+++ core/src/main/java/tech/tablesaw/aggregate/InstantAggregateFunction.java
@@ -7,17 +7,24 @@ import tech.tablesaw.api.InstantColumn;
 /** A partial implementation of aggregate functions to summarize over an instant column */
 public abstract class InstantAggregateFunction extends AggregateFunction<InstantColumn, Instant> {
 
+  /**
+   * Constructs an InstantAggregateFunction with the given name. The name is used as a column name
+   * in the output
+   */
   public InstantAggregateFunction(String name) {
     super(name);
   }
 
+  /** Returns an Instant that is the result of applying this function to the given column */
   public abstract Instant summarize(InstantColumn column);
 
+  /** {@inheritDoc} */
   @Override
   public boolean isCompatibleColumn(ColumnType type) {
     return type.equals(ColumnType.INSTANT);
   }
 
+  /** {@inheritDoc} */
   @Override
   public ColumnType returnType() {
     return ColumnType.INSTANT;
