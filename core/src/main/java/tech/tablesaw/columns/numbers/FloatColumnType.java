52d0826b1ded2ab8321a59d653d0f6eebd9d4a11
Larry White <ljw1001@gmail.com>
2021-10-18 18:12:27 -0400

    Complete JavaDoc for FloatColumnType and IntColumnType

--- core/src/main/java/tech/tablesaw/columns/numbers/FloatColumnType.java
+++ core/src/main/java/tech/tablesaw/columns/numbers/FloatColumnType.java
@@ -5,10 +5,12 @@ import tech.tablesaw.api.FloatColumn;
 import tech.tablesaw.columns.AbstractColumnType;
 import tech.tablesaw.io.ReadOptions;
 
+/** The {@link ColumnType} for {@link FloatColumn} */
 public class FloatColumnType extends AbstractColumnType {
 
   public static final int BYTE_SIZE = 4;
 
+  /** Returns the default parser for {@link FloatColumn} */
   public static final FloatParser DEFAULT_PARSER = new FloatParser(ColumnType.FLOAT);
 
   private static FloatColumnType INSTANCE;
@@ -17,6 +19,7 @@ public class FloatColumnType extends AbstractColumnType {
     super(byteSize, name, printerFriendlyName);
   }
 
+  /** Returns the singleton instance of FloatColumnType */
   public static FloatColumnType instance() {
     if (INSTANCE == null) {
       INSTANCE = new FloatColumnType(BYTE_SIZE, "FLOAT", "float");
@@ -24,20 +27,24 @@ public class FloatColumnType extends AbstractColumnType {
     return INSTANCE;
   }
 
+  /** {@inheritDoc} */
   @Override
   public FloatColumn create(String name) {
     return FloatColumn.create(name);
   }
 
+  /** {@inheritDoc} */
   @Override
   public FloatParser customParser(ReadOptions options) {
     return new FloatParser(this, options);
   }
 
+  /** Returns true if the given value is the missing value indicator for this column type */
   public static boolean valueIsMissing(float value) {
     return Float.isNaN(value);
   }
 
+  /** Returns the missing value indicator for this column type */
   public static float missingValueIndicator() {
     return Float.NaN;
   }
