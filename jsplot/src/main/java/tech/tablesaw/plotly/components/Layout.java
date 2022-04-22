fda5f30d2c94cae103c29bb5b491660a73c7061f
Nicolas Zerbib <nicolas.zerbib95@gmail.com>
2022-04-22 12:33:39 +0200

    Update Layout.java
    The default value for overmode in plotly is not FALSE but CLOSEST.
    The actual behavior prevent us from using FALSE hovermode

--- jsplot/src/main/java/tech/tablesaw/plotly/components/Layout.java
+++ jsplot/src/main/java/tech/tablesaw/plotly/components/Layout.java
@@ -21,7 +21,7 @@ public class Layout {
   private static final String DEFAULT_DECIMAL_SEPARATOR = ".";
   private static final String DEFAULT_THOUSANDS_SEPARATOR = ",";
   private static final boolean DEFAULT_AUTO_SIZE = false;
-  private static final HoverMode DEFAULT_HOVER_MODE = HoverMode.FALSE;
+  private static final HoverMode DEFAULT_HOVER_MODE = HoverMode.CLOSEST;
   private static final DragMode DEFAULT_DRAG_MODE = DragMode.ZOOM;
   private static final int DEFAULT_HOVER_DISTANCE = 20;
   private static final BarMode DEFAULT_BAR_MODE = BarMode.GROUP;
