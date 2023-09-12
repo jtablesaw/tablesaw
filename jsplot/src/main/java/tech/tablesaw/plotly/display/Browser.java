4a90a55ad6f843886aabe93bc06fd058ac5d80d1
Omar Allfuraydi <carboomar122@gmail.com>
2023-09-12 16:25:33 +0300

    Formatted the code

--- jsplot/src/main/java/tech/tablesaw/plotly/display/Browser.java
+++ jsplot/src/main/java/tech/tablesaw/plotly/display/Browser.java
@@ -7,25 +7,24 @@ import java.net.URI;
 
 public class Browser {
 
-  public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-    if (Desktop.isDesktopSupported()) {
-      Desktop.getDesktop().browse(new URI("http://www.example.com"));
+        if (Desktop.isDesktopSupported()) {
+            Desktop.getDesktop().browse(new URI("http://www.example.com"));
+        }
     }
-  }
 
-  public void browse(File file) throws IOException {
-    if (Desktop.isDesktopSupported()) {
-      if(Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)){
-      Desktop.getDesktop().browse(file.toURI());}
-     else if (Desktop.getDesktop().isSupported(Desktop.Action.OPEN)){
-        Desktop.getDesktop().open(file);
-     }else{
-         throw new UnsupportedOperationException("Neither open nor browse are supported");
-     }
+    public void browse(File file) throws IOException {
+        if (Desktop.isDesktopSupported()) {
+            if (Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+                Desktop.getDesktop().browse(file.toURI());
+            } else if (Desktop.getDesktop().isSupported(Desktop.Action.OPEN)) {
+                Desktop.getDesktop().open(file);
+            } else {
+                throw new UnsupportedOperationException("Neither open nor browse are supported");
+            }
+        } else {
+            throw new UnsupportedOperationException("Browser not supported.");
+        }
     }
-     else {
-      throw new UnsupportedOperationException("Browser not supported.");
-    }
-  }
 }
