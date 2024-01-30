e095f4667f2ed1fa56c0b9aa7eab49c8766b2507
Nick <53413353+nickpalladino@users.noreply.github.com>
2024-01-30 14:45:23 -0500

    Update missing value test

--- core/src/test/java/tech/tablesaw/io/csv/CsvReaderTest.java
+++ core/src/test/java/tech/tablesaw/io/csv/CsvReaderTest.java
@@ -525,7 +525,7 @@ public class CsvReaderTest {
   void testWithMissingValue2() throws IOException {
 
     Table t = Table.read().csv("../data/missing_values2.csv");
-    assertEquals(0, t.stringColumn(0).countMissing());
+    assertEquals(1, t.stringColumn(0).countMissing());
     assertEquals(0, t.numberColumn(1).countMissing());
     assertEquals(0, t.numberColumn(2).countMissing());
   }
