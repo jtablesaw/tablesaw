d1acdce059b7e5da84a816640ef45d18f5df433d
igrqb <100824876+igrqb@users.noreply.github.com>
2024-03-13 11:36:15 +1000

    upgrade pebble and associated classes. added versions-maven-plugin - usage mvn versions:display-dependency-updates

--- jsplot/src/main/java/tech/tablesaw/plotly/traces/HistogramTrace.java
+++ jsplot/src/main/java/tech/tablesaw/plotly/traces/HistogramTrace.java
@@ -1,7 +1,7 @@
 package tech.tablesaw.plotly.traces;
 
-import com.mitchellbosecke.pebble.error.PebbleException;
-import com.mitchellbosecke.pebble.template.PebbleTemplate;
+import io.pebbletemplates.pebble.error.PebbleException;
+import io.pebbletemplates.pebble.template.PebbleTemplate;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.UncheckedIOException;
