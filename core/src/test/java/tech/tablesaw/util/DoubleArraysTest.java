f85029bb86f62db344af40eda6f5b3a03596de4c
ccleva <christophe.cleva@orange.fr>
2025-01-04 10:19:08 +0100

    Fix code style

--- core/src/test/java/tech/tablesaw/util/DoubleArraysTest.java
+++ core/src/test/java/tech/tablesaw/util/DoubleArraysTest.java
@@ -14,8 +14,8 @@
 
 package tech.tablesaw.util;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
@@ -29,8 +29,8 @@ public class DoubleArraysTest {
     Table table = Table.read().csv("../data/tornadoes_1950-2014.csv");
     TableSliceGroup tableSliceGroup = table.splitOn("Scale");
     int columnNumber = table.columnIndex("Injuries");
-    final double[][] a2dArray = DoubleArrays.to2dArray(tableSliceGroup, columnNumber);
-    assertEquals(7, a2dArray.length, "Wrong Scale array length");
+    double[][] a2dArray = DoubleArrays.to2dArray(tableSliceGroup, columnNumber);
+    assertEquals(7, a2dArray.length);
   }
 
   @Test
